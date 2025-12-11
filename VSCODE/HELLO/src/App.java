a3879b6d79e1802955701cd53909a505c483ef7d
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-12-10 23:13:15 -0300

    programa simples com scanner

--- VSCODE/HELLO/src/App.java
+++ VSCODE/HELLO/src/App.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         //
-        Scanner scanner; 
-        //asdasd
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Olá, informe seu nome:\n");
+        String name = scanner.next();
+        System.out.println("Informe o sua idade:\n");
+        int age = scanner.nextInt();
+        System.out.println("Olá " + name + ", você tem " + age + " anos!");
+
         
     }
 }
