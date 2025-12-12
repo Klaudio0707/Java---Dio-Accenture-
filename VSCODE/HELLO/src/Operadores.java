5dbaf0f6cb243766afe3cca05203f07a1047db44
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-12-11 23:28:24 -0300

    operadores

--- VSCODE/HELLO/src/Operadores.java
+++ VSCODE/HELLO/src/Operadores.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Operadores {
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Quanto é 2 + 2?\n ");
-        var result = scanner.nextInt();
-        var isWrong = result != 4;
-        System.out.printf("Voce Acertou? (%s)\n", !isWrong);
+     public static void main(String[] args) {
+   
+        System.out.printf("true && true = %s\n", true && true); // true
+        System.out.printf("true && true = %s\n", true && true); // true
+        System.out.printf("true && false = %s\n", true && false); // false
+        System.out.printf("false && false = %s\n", false && false); // false
+        System.out.println("========================================");
+        System.out.printf("true || true = %s\n", true || true); // true
+        System.out.printf("true || false = %s\n", true || false); // true
+        System.out.printf("false || true = %s\n", false || true); // true
+        System.out.printf("false || false = %s\n", false || false); // false
+        System.out.println("========================================");
+        
     }
+
 }
