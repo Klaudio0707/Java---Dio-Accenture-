20aae232ebb41d1497763e3da7e3c5d077da7ca0
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-12-12 23:23:06 -0300

    exercicio 1 - nome e idade

---
+++ VSCODE/HELLO/src/Exercicio_1.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe seu nome: ");
+        var nome = scanner.nextLine();
+        System.out.printf("%s, Informe o seu ano de nascimento: \n", nome);
+        var anoNascimento = scanner.nextInt();
+        var idade = 2025 - anoNascimento;
+        System.out.printf("%s, você tem %d anos!", nome, idade);
+        scanner.close();
+    }
+}
