027a0b3a823baf973e89125aa0008e832cc0e146
klaudio0707 <claudioroberto0707@outlook.com>
2025-12-17 18:02:50 -0300

    atividade calculadora imc

--- VSCODE/HELLO/src/Exercicio_2.java
+++ VSCODE/HELLO/src/Exercicio_2.java
@@ -4,8 +4,8 @@ public class Exercicio_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Informe quantos centimetros, tem um lado do quadrado:");
-        var lado = scanner.nextInt();
-        var areaQuadrado = lado*lado;
+         int lado = scanner.nextInt();
+        int areaQuadrado = lado*lado;
         System.out.printf("Area total do quadrado é: %d cm²", areaQuadrado);
         }
 }
